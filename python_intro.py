1d733bb875092208684ba3178c468efb9c482ce8
Coop <cooper.kimberly@gmail.com>
2017-10-07 17:19:26 -0500

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,10 @@
+girls = ["Andrea", "Lily", "Ethel", "Kendall", "Lindsey", "Amity", "You"]
+def hi(name):
+    print("Hi " + name + "!")
+hi("Welcome New Django Girl")
+for name in girls:
+    hi(name)
+    print("Next girl")
+for i in range(1, 6):
+    print(i)
+    
